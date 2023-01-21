fe3628a47d38de75baf26da1a2eb7dff6e1d6c98
Danushka Pathirana <pathirana.danushka.rp@gmail.com>
2023-01-21 12:01:55 +0530

    develop handling errors when fetching data

--- src/App.js
+++ src/App.js
@@ -7,12 +7,20 @@ import classes from './App.css'
 const App = () => {
   const [movies, setMovies] = useState([])
   const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
 
   const fetchMovieHandler = () => {
     setIsLoading(true)
+    setError(null)
+
     fetch('https://swapi.dev/api/films')
     .then((response) => {
-      return response.json() //transform the response into real js object
+      if(response.ok) {
+        return response.json() //transform the response into real js object
+      }
+      else {
+        throw new Error('Something went wrong.!')
+      }
     })
     .then((data) => {
       const transformedMovies = data.results.map((movieData) => { //mapping names that we have used in our application and names that used in API
@@ -24,9 +32,41 @@ const App = () => {
         }
       })
       setMovies(transformedMovies)
-      setIsLoading(false)
     })
+    .catch((error) => {
+      setError(error.message)
+    })
+    setIsLoading(false)
   }
+
+  // async function fetchMovieHandler() {
+  //   setIsLoading(true)
+  //   setError(null)
+
+  //   try {
+  //     const response = await fetch('https://swapi.dev/api/films')
+      
+  //     if(!response.ok) {
+  //       throw new Error('Something went wrong.!')
+  //     }
+
+  //     const data = await response.json()
+      
+  //     const transformedMovies = data.results.map((movieData) => {
+  //       return{
+  //         id: movieData.episode_id,
+  //         title: movieData.title,
+  //         openingText: movieData.opening_crawl,
+  //         releaseDate: movieData.release_date
+  //       }
+  //     })
+  //     setMovies(transformedMovies)
+  //   }
+  //   catch(error) {
+  //     setError(error.message)
+  //   }
+  //   setIsLoading(false)
+  // }
 
   // using async / await syntax
   // async const fetchMovieHandler  = () => {
@@ -41,7 +81,8 @@ const App = () => {
       </section>
       <section>
         {!isLoading && movies.length > 0 && <MovieList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>No movies found.!</p> }
+        {!isLoading && movies.length === 0 && !error && <p>No movies found.!</p> }
+        {!isLoading && error && <p>{error}</p>}
         {isLoading && <p>Loading...</p>}
       </section>
     </Fragment>
